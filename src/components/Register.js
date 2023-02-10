3cf80c1068e04a6bc27791164b9f4b5f4f365a54
Cheilanthe <65095040+Cheilanthe@users.noreply.github.com>
2023-02-10 13:58:39 -0600

    agrega la promesa de register 10/02

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,9 +1,16 @@
-import { registerEvent } from '../lib/fireFunction.js';
+import { registerEvent, loginGoogle } from '../lib/fireFunction.js';
 
-export const register = (onNavigate) => {
+export const register = () => {
   const HomeDiv = document.createElement('div');
-  HomeDiv.textContent = 'Registro';
   const logoDiv = document.createElement('div');
+  const RegTitleDiv = document.createElement('div');
+  const containerRegister = document.createElement('div');
+  containerRegister.setAttribute('class', 'containerRegis');
+  containerRegister.setAttribute('id', 'containerRegis');
+  const divError = document.createElement('div');
+  const form = document.createElement('form');
+  form.setAttribute('class', 'form');
+  form.setAttribute('id', 'form');
   logoDiv.setAttribute('class', 'logoDiv');
   const logo = document.createElement('img');
   logo.setAttribute('src', '/img/logo.png');
@@ -12,61 +19,138 @@ export const register = (onNavigate) => {
   const title = document.createElement('h2');
   title.setAttribute('class', 'title');
   title.textContent = 'PETGRAM';
-  // formulario
+  RegTitleDiv.textContent = 'Registro';
+  // formulario etiquetas
   const labelUser = document.createElement('label');
-  labelUser.setAttribute('class', 'labels');
-  labelUser.textContent = 'Usuario';
-  const labelDate = document.createElement('label');
-  labelDate.setAttribute('class', 'labels');
-  labelDate.textContent = 'Fecha de Nacimiento';
-  const labelSex = document.createElement('label');
-  labelSex.setAttribute('class', 'labels');
-  labelSex.textContent = 'Sexo';
   const labelEmail = document.createElement('label');
-  labelEmail.setAttribute('class', 'labels');
-  labelEmail.textContent = 'Correo';
   const labelPass = document.createElement('label');
-  labelPass.setAttribute('class', 'labels');
-  labelPass.textContent = 'Contraseña';
+  // formulario inputs
   const inputUser = document.createElement('input');
+  const inputEmail = document.createElement('input');
+  const inputPass = document.createElement('input');
+  // texcontent de etiquetas e inputs
+  labelUser.textContent = 'Usuario';
+  labelEmail.textContent = 'Correo';
+  labelPass.textContent = 'Contraseña';
+  // abributos label
+  labelUser.setAttribute('class', 'labels');
+  labelEmail.setAttribute('class', 'labels');
+  labelPass.setAttribute('class', 'labels');
+  // atributos inputs
   inputUser.setAttribute('type', 'text');
   inputUser.setAttribute('class', 'inputs');
-  const inputDate = document.createElement('input');
-  inputDate.setAttribute('type', 'text');
-  inputDate.setAttribute('class', 'inputs');
-  const inputSex = document.createElement('input');
-  inputSex.setAttribute('type', 'text');
-  inputSex.setAttribute('class', 'inputs');
-  const inputEmail = document.createElement('input');
+  inputUser.setAttribute('id', 'user');
+  // inputUser.required = 'true';
   inputEmail.setAttribute('type', 'text');
   inputEmail.setAttribute('class', 'inputs');
-  const inputPass = document.createElement('input');
+  inputEmail.setAttribute('id', 'email');
+  // inputEmail.required = 'true';
   inputPass.setAttribute('type', 'text');
   inputPass.setAttribute('class', 'inputs');
-
+  inputPass.setAttribute('id', 'pass');
+  // inputPass.required = 'true';
   // botones
   const buttonCreate = document.createElement('button');
   buttonCreate.setAttribute('class', 'buttonCreate');
+  // poner un href o a tyope button para redirigir el código
   buttonCreate.textContent = 'Crear cuenta';
   const buttonGmail = document.createElement('button');
   buttonGmail.setAttribute('class', 'buttonGmail');
   buttonGmail.textContent = 'Ingresar con Google';
 
-  buttonCreate.addEventListener('click', () => registerEvent(inputEmail.value, inputPass.value));
-  buttonGmail.addEventListener('click', () => onNavigate('/'));
+  // promesas
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const userValue = inputUser.value;
+    const mailValue = inputEmail.value;
+    const passValue = inputPass.value;
+    const exprReg = /[A-z]/g;
+
+    function showError(userValue, divError) {
+      if (exprReg.test(userValue)) {
+        divError.innerHTML('válido');
+        inputUser.style.borderColor = 'green';
+      } else {
+        divError.innerHTML('inválido');
+        inputUser.style.borderColor = 'red';
+      }
+    }
+    if (userValue === '' || userValue.length < 5) {
+      alert('El usuario debe ser mayor a 5 caracteres');
+    } else if (userValue && mailValue && passValue) {
+      registerEvent(mailValue, passValue)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          // si la promesa es positiva debería redirigirme al login router
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log('errorcode', errorCode, 'errormessage', errorMessage);
+          // ..
+          if (errorCode) {
+            if (errorCode === 'auth/invalid-email') {
+              alert('Correo inválido');
+            } else if (errorCode === 'auth/email-already-in-use') {
+              alert('Correo inválido, ya esta en uso');
+            } else if (errorCode === 'auth/weak-password') {
+              alert('su contraseña es débil, ponga al menos 6 caracteres');
+            }
+          }
+        });
+    }
+  });
+
+  buttonGmail.addEventListener('click', () => loginGoogle('/'));
+
+
   logoDiv.append(logo, title);
-  HomeDiv.append(logoDiv);
-  HomeDiv.appendChild(labelUser);
-  HomeDiv.appendChild(inputUser);
+  form.append(labelUser, inputUser, divError, labelEmail, inputEmail);
+  form.append(labelPass, inputPass, buttonCreate, buttonGmail);
+  containerRegister.append(form);
+  HomeDiv.append(logoDiv, RegTitleDiv, containerRegister);
+  return HomeDiv;
+};
+
+
+/* optiones sexo y fecha de nac. 
+const labelDate = document.createElement('label');
+const labelSex = document.createElement('label');
+const inputDate = document.createElement('input');
+const inputSex = document.createElement('select');
+labelDate.setAttribute('class', 'labels');
+  labelSex.setAttribute('class', 'labels');
+inputDate.setAttribute('type', 'text');
+  inputDate.setAttribute('class', 'inputs');
+  inputDate.setAttribute('id', 'date');
+  inputDate.setAttribute('placeholder', 'DD / MM / AA');
+// select para el sexo
+  inputSex.setAttribute('class', 'inputs');
+  inputSex.setAttribute('id', 'sex');
+  // opción vacía
+  const optionSexEmpy = document.createElement('option');
+  optionSexEmpy.setAttribute('value', '');
+  optionSexEmpy.setAttribute('selected', 'true');
+  const textSexEmpy = document.createTextNode('seleccione');
+  optionSexEmpy.appendChild(textSexEmpy);
+  inputSex.appendChild(optionSexEmpy);
+  // opción macho
+  const optionSexM = document.createElement('option');
+  optionSexM.setAttribute('value', 'macho');
+  optionSexM.setAttribute('selected', 'true');
+  const textSexM = document.createTextNode('Macho');
+  optionSexM.appendChild(textSexM);
+  inputSex.appendChild(optionSexM);
+  // opción hembra
+  const optionSexH = document.createElement('option');
+  optionSexH.setAttribute('value', 'hembra');
+  optionSexH.setAttribute('selected', 'true');
+  const textSexH = document.createTextNode('Hembra');
+  optionSexH.appendChild(textSexH);
+  inputSex.appendChild(optionSexH);
+  // inputSex.required = 'true';
   HomeDiv.appendChild(labelDate);
   HomeDiv.appendChild(inputDate);
   HomeDiv.appendChild(labelSex);
-  HomeDiv.appendChild(inputSex);
-  HomeDiv.appendChild(labelEmail);
-  HomeDiv.appendChild(inputEmail);
-  HomeDiv.appendChild(labelPass);
-  HomeDiv.appendChild(inputPass);
-  HomeDiv.appendChild(buttonCreate);
-  HomeDiv.appendChild(buttonGmail);
-  return HomeDiv;
-};
+  HomeDiv.appendChild(inputSex); */
--- src/lib/fireFunction.js
+++ src/lib/fireFunction.js
@@ -4,6 +4,8 @@ import {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
 } from 'firebase/auth';
 import { firebaseConfig } from './firebaseConfig.js';
 
@@ -11,17 +13,7 @@ export const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 export function registerEvent(email, password) {
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed in
-      const user = userCredential.user;
-      // ...
-    });
-  // .catch((error) => {
-  //   const errorCode = error.code;
-  //   const errorMessage = error.message;
-  //   // ..
-  // });
+  return createUserWithEmailAndPassword(auth, email, password)
 }
 
 export function login(email, password) {
@@ -47,3 +39,26 @@ export function login(email, password) {
 //     // ...
 //   }
 // });
+
+export function loginGoogle() {
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+    }).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+}
