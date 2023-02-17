b35a579205842772a6ec32b524c81b6a0a4e3704
Andrea Flores <118763091+AndreaFlo1794@users.noreply.github.com>
2023-02-17 17:32:20 -0500

    estructuración Muro 17.02

--- src/components/Muro.js
+++ src/components/Muro.js
@@ -1,32 +1,76 @@
 export const muro = () => {
-  const HomeDiv = document.createElement('div');
+  const HomeDivMuro = document.createElement('div');
   const logoDiv = document.createElement('div');
   const logo = document.createElement('img');
   const title = document.createElement('h2');
+  const divContainer = document.createElement('div');
   const writeMuro = document.createElement('div');
   const write = document.createElement('input');
   const post = document.createElement('button');
   const postMuro = document.createElement('div');
-  const button = document.createElement('button');
+  const likeDiv = document.createElement('div');
+  const logoLike = document.createElement('img');
+  const postSecond = document.createElement('button');
+  const postMuroSecond = document.createElement('div');
+  const likeSecond = document.createElement('div');
+  const logoSecond = document.createElement('img');
+  const buttonProfile = document.createElement('button');
+  const logoProfile = document.createElement('img');
   const buttonHome = document.createElement('button');
+  const logoButton = document.createElement('img');
+  const publishDiv = document.createElement('div');
+  const buttonPublish = document.createElement('button');
+  const buttonContainer = document.createElement('div');
 
+  HomeDivMuro.setAttribute('class', 'HomeDivMuro');
   logoDiv.setAttribute('class', 'logoMuro');
   logo.setAttribute('src', '/img/logo.png');
   logo.setAttribute('alt', 'logoAlt');
-  logo.setAttribute('class', 'titleMuro');
+  title.setAttribute('class', 'titleMuro');
+  divContainer.setAttribute('class', 'divContainer');
   writeMuro.setAttribute('class', 'writeMuro');
   write.setAttribute('class', 'write');
   post.setAttribute('class', 'post');
   postMuro.setAttribute('class', 'postMuro');
+  likeDiv.setAttribute('class', 'likeDiv');
+  logoLike.setAttribute('src', '/img/like.png');
+  logoLike.setAttribute('alt', 'logoAlt');
+  logoLike.setAttribute('class', 'logoLike');
+  postSecond.setAttribute('class', 'postSecond');
+  postMuroSecond.setAttribute('class', 'postMuroSecond');
+  likeSecond.setAttribute('class', 'likeSecond');
+  logoSecond.setAttribute('src', '/img/like.png');
+  logoSecond.setAttribute('alt', 'logoAlt');
+  logoSecond.setAttribute('class', 'logoSecond');
+  publishDiv.setAttribute('class', 'publishDiv');
+  buttonPublish.setAttribute('class', 'buttonPublish');
+  buttonHome.setAttribute('class', 'buttonHome');
+  logoButton.setAttribute('src', '/img/pet-love.png');
+  logoButton.setAttribute('alt', 'logoAlt');
+  buttonProfile.setAttribute('class', 'buttonProfile');
+  logoProfile.setAttribute('src', '/img/Max.jpeg');
+  logoProfile.setAttribute('alt', 'logoAlt');
+  buttonContainer.setAttribute('class', 'buttonContainer');
 
   title.textContent = 'PETGRAM';
-  button.textContent = 'perfil';
-  buttonHome.textContent = 'inicio';
+  buttonPublish.textContent = 'Publicar';
+
+  write.placeholder = '¿En qué estás pensando?';
 
   logoDiv.append(logo, title);
-  writeMuro.append();
+  divContainer.append(writeMuro, postMuro, postMuroSecond, buttonContainer);
+  writeMuro.append(write, publishDiv);
+  publishDiv.appendChild(buttonPublish);
+  buttonContainer.append(buttonHome, buttonProfile);
+  buttonHome.appendChild(logoButton);
+  buttonProfile.appendChild(logoProfile);
+  postMuroSecond.appendChild(likeSecond);
+  postMuro.appendChild(likeDiv);
+  likeDiv.appendChild(post);
+  likeSecond.appendChild(postSecond);
+  post.appendChild(logoLike);
+  postSecond.appendChild(logoSecond);
+  HomeDivMuro.append(logoDiv, divContainer);
 
-  HomeDiv.append(logoDiv, writeMuro, postMuro, button, buttonHome);
-
-  return HomeDiv;
+  return HomeDivMuro;
 };
